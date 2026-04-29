a54669ba874f0a8b897d1f66a617b0201faf4f6c
Omm579 <omdebasish.2006@gmail.com>
2026-04-29 15:05:39 +0530

    Initial Commit

--- components/FloatingCards.jsx
+++ components/FloatingCards.jsx
@@ -198,7 +198,7 @@ export default function FloatingCards({ setSection }) {
           <span className="text-gray-400">Hi,</span>
 
           <span className="bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-transparent font-semibold truncate max-w-[120px] sm:max-w-none">
-            {userName}
+            {user?.user_metadata?.name || data.name}
           </span>
 
           <span className="inline-block text-lg sm:text-xl scale-110 animate-[wave_2s_infinite] origin-[70%_70%]">
